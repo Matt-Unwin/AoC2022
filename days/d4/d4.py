12ff61fa5071dc185d6fe437bc0c300358e04c06
Matt Unwin <matthewunwin987@gmail.com>
2022-12-04 11:19:22 +0000

    Added alternative versions of day 4 tasks using set operations

--- days/d4/d4.py
+++ days/d4/d4.py
@@ -13,12 +13,36 @@ def pt1():
     print(total)
 
 
+def pt1_set_ops():
+    total = 0
+    for line in lines:
+        sp = re.split("[,-]", line)
+        sp = list(map(int, sp))  # convert list to int as otherwise I think it compares by alphabetical order
+        set1 = set(range(sp[0], sp[1] + 1))
+        set2 = set(range(sp[2], sp[3] + 1))
+        if set1.issubset(set2) or set2.issubset(set1):
+            total += 1
+    print(total)
+
+
 def pt2():
     total = 0
     for line in lines:
-        sp = re.split(",|-", line)
+        sp = re.split("[,-]", line)
         sp = list(map(int, sp))
         if (sp[2] <= sp[0] <= sp[3]) or (sp[2] <= sp[1] <= sp[3]) or (sp[0] <= sp[2] <= sp[1]) or (sp[0] <= sp[3] <= sp[1]):
+            total += 1
+    print(total)
+
+
+def pt2_set_ops():
+    total = 0
+    for line in lines:
+        sp = re.split("[,-]", line)
+        sp = list(map(int, sp))
+        set1 = set(range(sp[0], sp[1] + 1))
+        set2 = set(range(sp[2], sp[3] + 1))
+        if len(set1.intersection(set2)) > 0:
             total += 1
     print(total)
 
@@ -28,3 +52,7 @@ if __name__ == "__main__":
         lines = f.read().splitlines()
     pt1()
     pt2()
+
+    # set operation versions takes a little over double the time to execute but are more readable
+    pt1_set_ops()
+    pt2_set_ops()
