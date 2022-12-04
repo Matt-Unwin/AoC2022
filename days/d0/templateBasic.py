38c6bc3b5d9c409e75ea3cfe02809887a8df7272
Matt Unwin <Matt.Unwin@bjss.com>
2022-12-04 21:56:54 +0000

    Updated template

--- days/d0/templateBasic.py
+++ days/d0/templateBasic.py
@@ -1,4 +1,10 @@
-def main():
+def pt1():
+    for x in lines:
+        print(x, sep="", end="")
+    # code here
+
+
+def pt2():
     for x in lines:
         print(x, sep="", end="")
     # code here
@@ -7,4 +13,5 @@ def main():
 if __name__ == "__main__":
     with open("input.txt") as f:
         lines = f.read().splitlines()
-    main()
+    print("part1: " + str(pt1()))
+    print("part2: " + str(pt2()))
