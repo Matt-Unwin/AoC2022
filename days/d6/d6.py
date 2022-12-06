908b110dd1e276e55d916c8d81cc95f2df63439a
Matt Unwin <matthewunwin987@gmail.com>
2022-12-06 08:48:50 +0000

    D6 - reduced to single function for both tasks, and optimised

--- days/d6/d6.py
+++ days/d6/d6.py
@@ -1,21 +1,12 @@
-def pt1():
-    for i in range(3, len(line)):
-        test_set = {line[i], line[i - 1], line[i - 2], line[i - 3]}
-        if len(test_set) == 4:
-            return i + 1
-
-
-def pt2():
-    for i in range(12, len(line)):
-        test_set = set()
-        for x in range(0, 14):
-            test_set.add(line[i-x])
-        if len(test_set) == 14:
+def dif_seq_finder(seq_len):
+    for i in range(seq_len-1, len(line)):
+        test_set = set([x for x in line[i - (seq_len - 1): i+1]])
+        if len(test_set) == seq_len:
             return i + 1
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         line = f.read()
-    print("part1: " + str(pt1()))
-    print("part2: " + str(pt2()))
+    print("part1: " + str(dif_seq_finder(4)))
+    print("part2: " + str(dif_seq_finder(14)))
